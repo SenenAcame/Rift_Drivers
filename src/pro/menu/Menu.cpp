ab9c0bbb6df9ebaeeeb974cbca60ab1c29ea594d
AngelicaQuaranta <aqb8@alu.ua.es>
2022-03-06 20:19:25 +0100

    a falta de mostras las 3 opciones

---
+++ src/pro/menu/Menu.cpp
@@ -0,0 +1,62 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include "Menu.h"
+
+Menu::Menu(float width, float heigth)
+{
+    if(!font.loadFromFile("../resources/MontserratAlternates-Bold.otf"))
+    {
+        // error...
+    }
+    menu[0].setFont(font);
+    menu[0].setFillColor(sf::Color::Magenta);
+    //menu[0].setColor(sf::Color::Red);
+    menu[0].setString("Empezar");
+    menu[0].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+
+    menu[1].setFont(font);
+    menu[1].setFillColor(sf::Color::White);
+    //menu[1].setColor(sf::Color::White);
+    menu[1].setString("Opciones");
+    menu[1].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+
+    menu[2].setFont(font);
+    menu[2].setFillColor(sf::Color::White);
+    //menu[2].setColor(sf::Color::White);
+    menu[2].setString("Salir");
+    menu[2].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+
+}
+
+Menu::~Menu()
+{
+
+}
+
+void Menu::draw(sf::RenderWindow &window)
+{
+    for(int i = 0; i < MAX_NUMBER_OF_ITEMS; i++)
+    {
+        window.draw(menu[1]);
+    }
+}
+
+void Menu::MoveUp()
+{
+    if(selectedItemIdex - 1 >= 0)
+    {
+        menu[selectedItemIdex].setColor(sf::Color::White);
+        selectedItemIdex--;
+        menu[selectedItemIdex].setColor(sf::Color::Magenta);
+    }
+}
+
+void Menu::MoveDown()
+{
+    if(selectedItemIdex + 1 < MAX_NUMBER_OF_ITEMS)
+    {
+        menu[selectedItemIdex].setColor(sf::Color::White);
+        selectedItemIdex++;
+        menu[selectedItemIdex].setColor(sf::Color::Magenta);
+    }
+}
