83d89e49932dd74a4dae0f09ac2f43f306422c99
AngelicaQuaranta <aqb8@alu.ua.es>
2022-03-06 20:48:07 +0100

    cambiado los setColor por setFillColor

--- src/pro/menu/Menu.cpp
+++ src/pro/menu/Menu.cpp
@@ -12,19 +12,19 @@ Menu::Menu(float width, float heigth)
     menu[0].setFillColor(sf::Color::Magenta);
     //menu[0].setColor(sf::Color::Red);
     menu[0].setString("Empezar");
-    menu[0].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+    menu[0].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)*1));
 
     menu[1].setFont(font);
     menu[1].setFillColor(sf::Color::White);
     //menu[1].setColor(sf::Color::White);
     menu[1].setString("Opciones");
-    menu[1].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+    menu[1].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)*2));
 
     menu[2].setFont(font);
     menu[2].setFillColor(sf::Color::White);
     //menu[2].setColor(sf::Color::White);
     menu[2].setString("Salir");
-    menu[2].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)));
+    menu[2].setPosition(sf::Vector2f(width/2, heigth/(MAX_NUMBER_OF_ITEMS+1)*3));
 
 }
 
@@ -45,9 +45,9 @@ void Menu::MoveUp()
 {
     if(selectedItemIdex - 1 >= 0)
     {
-        menu[selectedItemIdex].setColor(sf::Color::White);
+        menu[selectedItemIdex].setFillColor(sf::Color::White);
         selectedItemIdex--;
-        menu[selectedItemIdex].setColor(sf::Color::Magenta);
+        menu[selectedItemIdex].setFillColor(sf::Color::Magenta);
     }
 }
 
@@ -55,8 +55,8 @@ void Menu::MoveDown()
 {
     if(selectedItemIdex + 1 < MAX_NUMBER_OF_ITEMS)
     {
-        menu[selectedItemIdex].setColor(sf::Color::White);
+        menu[selectedItemIdex].setFillColor(sf::Color::White);
         selectedItemIdex++;
-        menu[selectedItemIdex].setColor(sf::Color::Magenta);
+        menu[selectedItemIdex].setFillColor(sf::Color::Magenta);
     }
 }
