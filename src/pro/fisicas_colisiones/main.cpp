3f8902038b7d4ec924b2e3e2ebc98adda7f431bb
joanPasc <jpi9@gcloud.ua.es>
2022-03-08 14:48:33 +0100

    seguimos con colisiones

--- src/pro/fisicas_colisiones/main.cpp
+++ src/pro/fisicas_colisiones/main.cpp
@@ -5,7 +5,7 @@
 #include "ej_modulos/mimodulo.h"
 
 #define kVel 5
-
+/*
 bool collision(sf::Image &image1, sf::Image &image2, int x1, int x2, int y1, int y2){
   sf::Color color[2];
   bool collision=false;
@@ -21,14 +21,7 @@ bool collision(sf::Image &image1, sf::Image &image2, int x1, int x2, int y1, int
   }
   return collision;
 }
-bool colision(sf::Sprite sprite, sf::Sprite sprite2){
-  sf::Vector2f pos1=sprite.getPosition();
-  sf::Vector2f pos2=sprite2.getPosition();
-  if((pos1.x+75)>pos2.x && (pos1.y+75)>pos2.y && (pos2.x+75)>pos1.x && (pos2.y+75)>pos1.y){
-    return true;
-  }
-  return false;
-}
+*/
 
 int main() {
 
@@ -39,7 +32,7 @@ int main() {
 
   //Cargo la imagen donde reside la textura del sprite
   sf::Texture tex;
-  if (!tex.loadFromFile("resources/sprites.png")) {
+  if (!tex.loadFromFile("/home/alu/node/proyectoabp-grupo-c04/src/pro/fisicas_colisiones/resources/sprites.png")) {
     std::cerr << "Error cargando la imagen sprites.png";
     exit(0);
   }
@@ -56,8 +49,8 @@ int main() {
   sprite2.setTextureRect(sf::IntRect(4.5 * 75, 0 * 75, 75, 75));
 
   //prueba de poner hitbox
-  sf::Vector2f pos1=sprite.getPosition();
-  sf::Vector2f pos2=sprite2.getPosition();
+  //sf::Rect pos1=sprite.getGlobalBounds();
+  //sf::Rect pos2=sprite2.getGlobalBounds();
 
   // Lo dispongo en el centro de la pantalla
   sprite.setPosition(320, 240);
@@ -84,41 +77,45 @@ int main() {
 
         //Mapeo del cursor
         case sf::Keyboard::Right:
-          if(!colision(sprite, sprite2)){
             sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
             sprite2.setTextureRect(sf::IntRect(1 * 75, 2 * 75, 75, 75));
             //Escala por defecto
             sprite.setScale(1, 1);
             sprite2.setScale(1, 1);
             sprite.move(kVel, 0);
-          }
+            if(sprite.getGlobalBounds().intersects(sprite.getGlobalBounds())){
+              sprite.move(-kVel, 0);
+            }
           break;
 
         case sf::Keyboard::Left:
-          if(!colision(sprite, sprite2)){
             sprite.setTextureRect(sf::IntRect(0 * 75, 2 * 75, 75, 75));
             sprite2.setTextureRect(sf::IntRect(1 * 75, 2 * 75, 75, 75));
             //Reflejo vertical
             sprite.setScale(-1, 1);
             sprite2.setScale(-1, 1);
             sprite.move(-kVel, 0);
-          }
+            if(sprite.getGlobalBounds().intersects(sprite.getGlobalBounds())){
+              sprite.move(kVel, 0);
+            }
           break;
 
         case sf::Keyboard::Up:
-          if(!colision(sprite, sprite2)){
             sprite.setTextureRect(sf::IntRect(0 * 75, 3 * 75, 75, 75));
             sprite2.setTextureRect(sf::IntRect(1 * 75, 3 * 75, 75, 75));
             sprite.move(0, -kVel);
-          }
+            if(sprite.getGlobalBounds().intersects(sprite.getGlobalBounds())){
+                sprite.move(0, kVel);
+            }
           break;
 
         case sf::Keyboard::Down:
-          if(!colision(sprite, sprite2)){
             sprite.setTextureRect(sf::IntRect(0 * 75, 0 * 75, 75, 75));
             sprite2.setTextureRect(sf::IntRect(4.5 * 75, 0 * 75, 75, 75));
             sprite.move(0, kVel);
-          }
+            if(sprite.getGlobalBounds().intersects(sprite.getGlobalBounds())){
+                sprite.move(0, -kVel);
+            }
           break;
 
         //Tecla ESC para salir
