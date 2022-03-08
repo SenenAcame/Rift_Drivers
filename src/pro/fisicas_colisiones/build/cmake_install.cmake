ec8a099eab6dfada320701525b3530b282df2722
joanPasc <jpi9@gcloud.ua.es>
2022-03-08 14:13:22 +0100

    funcionan las colisiones

--- src/pro/fisicas_colisiones/build/cmake_install.cmake
+++ src/pro/fisicas_colisiones/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/osboxes/FV/proyectoabp-grupo-c04/src/pro/fisicas_colisiones
+# Install script for directory: /home/alu/node/proyectoabp-grupo-c04/src/pro/fisicas_colisiones
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/osboxes/FV/proyectoabp-grupo-c04/src/pro/fisicas_colisiones/build/ej_modulos/cmake_install.cmake")
+  include("/home/alu/node/proyectoabp-grupo-c04/src/pro/fisicas_colisiones/build/ej_modulos/cmake_install.cmake")
 
 endif()
 
@@ -51,5 +51,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/osboxes/FV/proyectoabp-grupo-c04/src/pro/fisicas_colisiones/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/alu/node/proyectoabp-grupo-c04/src/pro/fisicas_colisiones/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
