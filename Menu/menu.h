d2202645faf01119555cd2114f597a185e89eb71
Senén Martínez Acame <sma79@gcloud.ua.es>
2022-03-26 12:04:30 +0100

    Menu y main

---
+++ Menu/menu.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#define MAX_NUMBER_OF_ITEMS 3
+
+class Menu
+{
+    public:
+        //para cambiar de estado
+        static Menu* instance();
+        void update(float deltaTime);
+
+        void draw(sf::RenderWindow &window);
+        void MoveUp();
+        void MoveDown();
+        int GetPressedItem() {return selectedItemIdex; }
+        Menu(float width, float height);
+        ~Menu();
+
+    private:
+        int selectedItemIdex = 0;
+        sf::Font font;
+        sf::Text menu[MAX_NUMBER_OF_ITEMS];
+        sf::Texture textura;
+};
---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+//incluyo mi menu
+#include "Menu.h"
+
+#include "include/config.h"
+#include "ej_modulos/mimodulo.h"
+
+//#define kVel 5
+
+int main() 
+{
+  //creo la ventana
+  sf::RenderWindow window(sf::VideoMode(640, 680), "VENTANA MENU");
+ 
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)){
+
+}
+  Menu menu(window.getSize().x, window.getSize().y);
+  //creo singletons
+  //Transicion* trans = Transicion::instancia();
+
+  while(window.isOpen())
+  {
+    sf::Event event;
+
+    while(window.pollEvent(event))
+    {
+      switch (event.type)
+      {
+      case sf::Event::Closed:
+      window.close();
+      break;
+      
+
+      case sf::Event::KeyReleased:
+        switch (event.key.code)
+        {
+        case sf::Keyboard::Up:
+          menu.MoveUp();
+          break;
+        
+        case sf::Keyboard::Down:
+          menu.MoveDown();
+          break;
+
+        case sf::Keyboard::Key::Enter:
+          switch (menu.GetPressedItem()){
+            case 0:
+              std::cout <<"Has seleccionado emmpezar" << std::endl;
+              //Menu_empezar.draw(window);
+              //dibujo el otro menu
+                  //window.clear();
+              break;
+            case 1:
+              std::cout <<"Has seleccionado las opciones" << std::endl;
+              break;
+            case 2:
+              std::cout <<"salir" << std::endl;
+              window.close();
+              break;
+          }
+        break;
+        }
+      
+      
+      
+      }
+    }
+    window.clear();
+    menu.draw(window);
+    window.display();
+  }
+  //return 0;
+}
